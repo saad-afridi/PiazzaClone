49a8cbef7f87af93ddf751eaa893c9c8ea5ecbe1
Zereck12 <saadafridi2001@gmail.com>
2021-07-15 15:54:49 -0400

    Finished class schemas

--- server/models/class.py
+++ server/models/class.py
@@ -1,18 +1,55 @@
-from typing import Optional
-from pydantic import BaseModel, Field 
+from typing import Optional, List, Dict
+from pydantic import BaseModel, Field, EmailStr
 
 def ClassSchema(BaseModel):
-    name: str = Field(...)
-    number: str = Field(...)
+    class_name: str = Field(...)
+    class_num: str = Field(...)
     estimated_enroll: int = Field(..., gt=0)
     term: str = Field(...)
+    folders: List[str] = Field(...)
+    instructors: List[EmailStr] = Field(...)
+    post_num: int = 1
+    posts: List[object] = Field(...)
+    
     
     class Config:
         schema_extra = {
             "example": {
-                "name": "Intro. To Computer Science",
-                "number": "CSC148H5",
+                "class_name": "Intro. To Computer Science",
+                "class_num": "CSC148H5",
                 "estimated_enroll": 200,
-                "term": "FALL2020"
+                "term": "FALL2020",
+                "folders": ["midterm", "A1", "A2", "A3", "exam", "general"],
+                "instructors": ["ta1@utoronto.ca","ta2@utoronto.ca", "prof1@utoronto.ca"],
+                "posts_num": 2,
+                "posts": [
+                    {
+                        "index": 1,
+                        "type": "question",
+                        "post_to": "entire_class",
+                        "folders": "midterm",
+                        "summary": "Why was the midterm so hard?",
+                        "details": "I was stuck on q3 for most of the test and I didn't even have enough time to finish the whole thing"
+                    }
+                ]
             }
         }
+
+def UpdateClass(BaseModel):
+    class_name: Optional[str] = None
+    class_num: Optional[str] = None
+    estimated_enroll: Optional[int] = None
+    term: Optional[str] = None
+    folders: Optional[List[str]] = None
+    instructors: Optional[List[EmailStr]] = None
+    post_num: Optional[int] = None
+    posts: Optioanl[List[object]] = None
+    
+    class Config:
+        schema_extra: {
+            "example": {
+                "class_name": "Intro. To CS",
+                "term": "WINTER2020"
+            }
+        }
+    
