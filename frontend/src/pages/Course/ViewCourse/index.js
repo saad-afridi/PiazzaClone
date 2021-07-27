44774f50bd3452493935ec6d88df258d731c8f76
Zereck12 <saadafridi2001@gmail.com>
2021-07-27 12:10:26 -0400

    Progress on main page appbar

--- frontend/src/pages/Course/ViewCourse/index.js
+++ frontend/src/pages/Course/ViewCourse/index.js
@@ -7,6 +7,9 @@ import {
 	Toolbar,
 	ButtonGroup,
 	makeStyles,
+	FormControl,
+	Select,
+	MenuItem,
 } from '@material-ui/core';
 import { red } from '@material-ui/core/colors';
 
@@ -20,43 +23,96 @@ const useStyles = makeStyles((theme) => ({
 		color: '#FFFFFFCC',
 		backgroundColor: '#2d3748',
 	},
-    buttonGroups : {
-        marginLeft: 'auto',
-        marginRight: '10px'
-    }
+	buttonGroups: {
+		marginLeft: 'auto',
+		marginRight: '10px',
+	},
+	courseSelect: {
+		marginLeft: '20px',
+		marginRight: 'auto',
+	},
+	userType: {
+		marginRight: '20px',
+	},
 }));
+
+const selectCourse = (course, key) => {
+	return (
+		<MenuItem value={course} key={key}>
+			{course.toUpperCase()}
+		</MenuItem>
+	);
+};
 
 const ViewCourse = () => {
 	const classes = useStyles();
 	const history = useHistory();
-	const dispatch = useDispatch();
-	const { user } = useSelector((state) => state.userState);
 
-    // Handlers
+	const dispatch = useDispatch();
+	const state = useSelector((state) => state);
+	const { user } = state.userState;
+	const courses = state.courseState;
+
+	// makes a num to course dict so {CSC108: Course id ...}
+	let tmpCourseOpts = [];
+	const courseNumToCourse = courses.reduce((groupedCourse, course) => {
+		if (user.courses.includes(course.id)) {
+			groupedCourse[course.class_num] = course.id;
+			tmpCourseOpts.push(course.class_num);
+		}
+		return groupedCourse;
+	}, {});
+
+	const [courseOption, setCourseOption] = React.useState(tmpCourseOpts[0]);
+
+	// Handlers
 	const handleLogout = () => {
 		dispatch(logout());
 	};
 
-    const handleEnroll = () => {
-        history.push('/enroll')
-    }
+	const handleEnroll = () => {
+		history.push('/enroll');
+	};
 
-    const handleCreateCourse = () => {
-        history.push('/create-course')
-    }
+	const handleCreateCourse = () => {
+		history.push('/create-course');
+	};
 
+	console.log(courseOption, tmpCourseOpts[0], courseNumToCourse);
 	return (
 		<div>
 			<AppBar position="static" className={classes.appBar}>
 				<Toolbar>
 					<Typography variant="h6">PiazzaClone</Typography>
-					<ButtonGroup color="primary" className={classes.buttonGroups}>
+					<FormControl className={classes.courseSelect}>
+						<Select
+							value={courseOption}
+							onChange={(e) => setCourseOption(e.target.value)}>
+							{tmpCourseOpts.map((_course, _index) => {
+								return selectCourse(_course, _index);
+							})}
+						</Select>
+					</FormControl>
+					<Typography
+						className={classes.userType}
+						variant="h5"
+						color="secondary">
+						{user.category.toUpperCase()}
+					</Typography>
+					<Typography variant="h6"> {user.name} </Typography>
+					<ButtonGroup
+						color="primary"
+						className={classes.buttonGroups}>
 						<Button onClick={handleEnroll}>Enroll In Course</Button>
-						<Button onClick={handleCreateCourse} disabled={user.category === "student"}>
+						<Button
+							onClick={handleCreateCourse}
+							disabled={user.category === 'student'}>
 							Create Course
 						</Button>
 					</ButtonGroup>
-					<Button style={{backgroundColor: red[400]}} onClick={handleLogout}>
+					<Button
+						style={{ backgroundColor: red[400] }}
+						onClick={handleLogout}>
 						Logout
 					</Button>
 				</Toolbar>
